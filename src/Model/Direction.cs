28e969db032615947c8307b99e804911aa9bcfbd
Luke Mackie <cycro99@hotmail.com>
2016-04-09 10:51:55 +1000

    replacing vb files with cs files

---
+++ src/Model/Direction.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+/// <summary>
+/// The direction the ship can be oriented.
+/// </summary>
+public enum Direction
+{
+	/// <summary>
+	/// The ship is oriented left/right
+	/// </summary>
+	LeftRight,
+
+	/// <summary>
+	/// The ship is oriented up/down
+	/// </summary>
+	UpDown
+}
