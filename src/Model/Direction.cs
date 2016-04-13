2840e1e1f04776b0db0fc12a121158e20395e332
djflood123 <dj.flood@hotmail.com>
2016-04-13 13:59:59 +1000

    c#-conversion-master

---
+++ src/Model/Direction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyGame
+{
+	/// <summary>
+	/// The direction the ship can be oriented.
+	/// </summary>
+	public enum Direction
+	{
+		/// <summary>
+		/// The ship is oriented left/right
+		/// </summary>
+		LeftRight,
+
+		/// <summary>
+		/// The ship is oriented up/down
+		/// </summary>
+		UpDown
+	}
+}
