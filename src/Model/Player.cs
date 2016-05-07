13f213bae7a9ed1f3f57cfb68d235a1a2447a9d0
Krishna <4953193@student.swin.edu.au>
2016-05-07 12:35:23 +1000

    Shots not incremented

--- src/Model/Player.cs
+++ src/Model/Player.cs
@@ -193,7 +193,10 @@ public class Player : IEnumerable<Ship>
 			case ResultOfAttack.Miss:
 				_misses += 1;
 				break;
-		}
+            case ResultOfAttack.ShotAlready:
+                _shots -= 1;
+                break;
+        }
 
 		return result;
 	}
