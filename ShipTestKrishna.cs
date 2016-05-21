102bdf076ce7fb551564fffacf0cbc23ac473a7c
Krishna <4953193@student.swin.edu.au>
2016-05-22 02:07:49 +1000

    Unit test

---
+++ ShipTestKrishna.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MyGame
+{
+	[TestFixture ()]
+	public class ShipTestKrishna
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			Ship myShip = new Ship (ShipName.Submarine);
+			myShip.Hit ();
+
+			Assert.IsTrue (myShip.Hits == 1);
+			myShip.Hit ();
+			myShip.Hit ();
+			Assert.IsTrue (myShip.Hits == 3);
+		}
+	}
+}
+
